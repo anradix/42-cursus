ac21f70b3311d1ae9bdb143f1547d5d6f871ef63
Antoine RADIX <anradix@e3r10p8.42.fr>
2019-11-20 16:01:09 +0100

    update header in ft_printf.c

--- 3-ft_printf/srcs/stock/stock_o.c
+++ 3-ft_printf/srcs/stock/stock_o.c
@@ -6,11 +6,11 @@
 /*   By: anradix <anradix@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/19 15:47:45 by anradix           #+#    #+#             */
-/*   Updated: 2019/11/19 16:24:43 by anradix          ###   ########.fr       */
+/*   Updated: 2019/11/20 14:03:41 by anradix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../header/ft_printf.h"
+#include "ft_printf.h"
 
 void	stock_o(t_printf *p, uintmax_t nb)
 {
