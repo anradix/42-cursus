84c56c8376e3e56c0dbba77933da3b3ad7b07497
Antoine RADIX <anradix@e2r10p13.42.fr>
2019-11-21 21:11:01 +0100

    ft_printf valide 100%

--- 3-ft_printf/headers/ft_printf.h
+++ 3-ft_printf/headers/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: anradix <anradix@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/16 16:53:25 by anradix           #+#    #+#             */
-/*   Updated: 2019/11/19 17:18:43 by anradix          ###   ########.fr       */
+/*   Updated: 2019/11/21 14:03:51 by anradix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,6 @@ int		check_hash_o(t_printf *p, int prec);
 int		check_hash_x(t_printf *p, int prec, uintmax_t nb);
 void	padding(t_printf *p, int prec, int len, uintmax_t nb);
 void	u_padding(t_printf *p, int prec, int len, uintmax_t nb);
-
-/*
-** lib
-*/
 int		c_atoi(char *nptr);
 char	*c_itoa(t_printf *p, uintmax_t nb, size_t len, char c);
 int		c_nbrlen(uintmax_t nb, int base, int len);
