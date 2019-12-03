8cf8bba092df1ce1e5ec83ccec51bc53bfa7cb31
Antoine RADIX <anradix@e3r5p10.42.fr>
2019-12-03 19:30:37 +0100

    can open window and exit with esc

--- 5-cube3D/main.c
+++ 5-cube3D/main.c
@@ -6,7 +6,7 @@
 /*   By: anradix <anradix@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/03 16:06:04 by anradix           #+#    #+#             */
-/*   Updated: 2019/12/03 19:14:30 by anradix          ###   ########.fr       */
+/*   Updated: 2019/12/03 19:30:07 by anradix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 int		endop(t_struct *p)
 {
-		printf("yoooooooo");
-	//	mlx_destroy_window(p->mlx_ptr, p->win_ptr);
 		return (0);
 }
 
 int		deal_key(int key, void *param, t_struct *p)
 {
-	printf("%d\n", key);
+	p->state = key;
+	if (key == 53)
+	{
+		printf("EXIT\n");
+	//	mlx_destroy_window(p->mlx_ptr, p->win_ptr);
+		exit(0);
+	}
 	return (0);
 }
 
@@ -29,17 +33,15 @@ int		deal_key(int key, void *param, t_struct *p)
 int		main(int ac, char **av)
 {
 	t_struct p;
-
+	/////////////////////////////////
 	p.mlx_ptr = mlx_init();
 	if (p.mlx_ptr == NULL)
 		return (1);
-
-
+	////////////////////////////////
 	p.win_ptr = mlx_new_window(p.mlx_ptr, 500, 500, "TEST");
 	if (p.win_ptr == NULL)
 		return (1);
-
-
+	///////////////////////////////
 	mlx_key_hook(p.win_ptr, deal_key, (void*)0);
 	mlx_loop_hook(p.mlx_ptr, endop, (void*)0);
 	mlx_loop(p.mlx_ptr);
