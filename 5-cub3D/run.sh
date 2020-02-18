0c39b272f4737b153ddb8c844a0d5e415974a0db
Antoine Lorin <antoine@Antoines-MacBook-Pro.local>
2020-02-18 22:03:45 +0100

    clean

--- 5-cub3D/run.sh
+++
@@ -1,1 +0,0 @@
-cc -I mlx -I headers -I lib/header srcs/*.c lib/mylib.a -L mlx  -lmlx -framework OpenGL -framework AppKit -o cub3d && ./cub3d map.cub
--- cub3d/run.sh
+++
@@ -1,1 +0,0 @@
-cc -I mlx -I mylib/header *.c mylib/mylib.a -L mlx  -lmlx -framework OpenGL -framework AppKit -o cub3d && ./cub3d map1.cub
