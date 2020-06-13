d52f9d7f2276cc4bd1b7da0f31faf4071fcbbf7c
antoineradix <anradix@student.42.fr>
2020-06-13 19:11:12 +0200

    update dockerfile

--- 6-ft_server/srcs/wp-config.php
+++ 6-ft_server/srcs/wp-config.php
@@ -20,9 +20,9 @@
 /** Nom de la base de données de WordPress. */
 define('DB_NAME', 'wordpress');
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'wordpressuser');
+define('DB_USER', 'user42');
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '0000');
+define('DB_PASSWORD', 'user42');
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
